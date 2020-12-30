3efcf52123fa01de01b1f040d3385f1b5ef417f9
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-12-29 20:53:08 -0600

    Fixed issue with search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import './App.css';
 const App = () => {
 
     const [query, setQuery] = useState('')
+    const search = async(e)=>{
+        if (e.key ==='Enter'{
+            const data = await fetchWeather(query)
+            console.log(data)
+        })
+    }
 
     return ( <div className="main-container">
         <input type= "text"
@@ -13,6 +19,7 @@ const App = () => {
             placeholder="Search..."
             value={query}
             onChange={(e)=>setQuery(e.target.value)}
+            onKeyPress= {search}
             />
     </div>  )
 }
