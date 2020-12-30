febb0017ca53756bdace79ecd6e095402c8a0c57
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-12-29 21:15:20 -0600

    Update dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react'
 
-import {fetchWeather} from './api/fetchWeather'
+import { fetchWeather } from './api/fetchWeather'
 import './App.css';
 
 const App = () => {
 
     const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
     const search = async(e)=>{
-        if (e.key ==='Enter'{
+        if (e.key ==='Enter'){
             const data = await fetchWeather(query)
             console.log(data)
-        })
+            setWeather(data)
+            setQuery('')
+        }
+
     }
 
     return ( <div className="main-container">
@@ -21,6 +25,15 @@ const App = () => {
             onChange={(e)=>setQuery(e.target.value)}
             onKeyPress= {search}
             />
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+
+                    </h2>
+                </div>
+            )}
     </div>  )
 }
 
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 const API_KEY = '000d564c7c5a854477de5ec196ea98d2'
 const URL = 'https://api.openweathermap.org/data/2.5/weather'
-const fetchWeather = async (query) => {
-            const response = await.get(URL, {
+export const fetchWeather = async (query) => {
+            const {data} = await axios.get(URL, {
                     params: {
                         q: query,
                         unit: 'metric',
                         APPID: API_KEY
                     }
+                }
                 );
-                return data
+                return {data}
             }
